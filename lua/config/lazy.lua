64f4634b601a1bf2e8faf9580400c31d8d207522
clancey <clanceylu@gmail.com>
2024-03-22 14:23:58 +0800

    add extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.ui.edgy" },
+    { import = "lazyvim.plugins.extras.editor.outline" },
+    { import = "lazyvim.plugins.extras.editor.navic" },
     -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
     -- import/override with your plugins
     { import = "plugins" },
