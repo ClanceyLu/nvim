74267e040264af898c39ac6c7c14f760522c2b62
clancey <clanceylu@gmail.com>
2024-03-22 14:24:52 +0800

    fold support

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- local opt = vim.opt
+local opt = vim.opt
 
--- opt.foldmethod = "expr"
--- opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
 -- opt.nofoldenable = true
--- opt.foldlevel = 99
+opt.foldlevel = 99
+-- vim.o.foldcolumn = "1" -- '0' is not bad
+-- vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+-- vim.o.foldlevelstart = 99
+-- vim.o.foldenable = true
