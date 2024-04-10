db37e4dfb05a843dda0cc9ec4f9d5224cc68f7bc
clancey <clanceylu@gmail.com>
2024-04-10 16:45:33 +0800

    add project session and disable dasboard

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -16,6 +16,23 @@ return {
         -- workspaces_file = "path/to/file",          -- Path to workspaces json file
         -- sessions_directory = "path/to/dir",        -- Directory where sessions are stored
       })
+      -- Autostore session on VimExit
+      local Session = require("projections.session")
+      vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
+        callback = function()
+          Session.store(vim.loop.cwd())
+        end,
+      })
+
+      -- Switch to project if vim was started in a project dir
+      local switcher = require("projections.switcher")
+      vim.api.nvim_create_autocmd({ "VimEnter" }, {
+        callback = function()
+          if vim.fn.argc() == 0 then
+            switcher.switch(vim.loop.cwd())
+          end
+        end,
+      })
     end,
     keys = {
       { "<leader>fp", "<Cmd>Telescope projections<CR>", desc = "Projects" },
