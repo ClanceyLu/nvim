6a8cd10279b588865c49a349e10f911f7b9bd175
clancey <clanceylu@gmail.com>
2024-04-10 16:42:39 +0800

    gui 复制粘贴

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,3 +19,18 @@ map("n", "<leader>fs", "<cmd>:update<cr>", { desc = "Save" })
 
 map("n", "<leader>wv", "<cmd>:vsplit<cr>", { desc = "split window right" })
 map("n", "<leader>ws", "<cmd>:split<cr>", { desc = "split window below" })
+
+-- for neovide
+if vim.g.neovide then
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+end
+
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
